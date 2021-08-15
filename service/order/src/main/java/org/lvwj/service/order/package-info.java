c96ca950841d894c01a5c05363b617c07f97d161
Lvwj <546694513@qq.com>
2021-08-15 13:25:20 +0800

    补充文字说明

---
+++ service/order/src/main/java/org/lvwj/service/order/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * domain:领域层，业务核心逻辑
+ * inbound:输入端(北向网关)
+ * outbound:输入端(南向网关)
+ * infra(infrastructure):基础设施层
+ *
+ * @author lvweijie
+ * @date 2021-08-15 12:33
+ */
+package org.lvwj.service.order;
